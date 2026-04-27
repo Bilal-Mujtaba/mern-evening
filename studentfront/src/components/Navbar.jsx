477021fafe094ed5502edcd60e7a595a320ef7c2
faizan <62203593+faizan11222@users.noreply.github.com>
2026-04-27 19:12:45 +0500

    update 1

---
+++ studentfront/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const Navbarr = () => {
+    return(
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/abc">Student Management System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
+}
+
+export default Navbarr;
--- studentfront/src/main.jsx
+++ studentfront/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//import react-bootstrap for the whole project
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
